1b86d4f1d07e2843709bd11bc96aa1d422774d2c
Sahra Bile <sahrabile@MacBook-Pro-som-tillhor-Sahra.local>
2023-12-07 15:23:01 +0100

    chore: Clean up

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import { productRoutes } from "./src/routes/product.routes";
 import { categoryRoutes } from "./src/routes/category.routes";
+import {colorRoutes} from './src/routes/color.routes'
 
 const app: Application = express();
 const port = process.env.PORT || 9000;
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/products", productRoutes);
 app.use("/categories", categoryRoutes);
+app.use("/colors", colorRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("We are on home page!");
