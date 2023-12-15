fb192da060effbc3de4b535b5cd7933b090a5dd5
Sahra Bile <sahrabile@MacBook-Pro-som-tillhor-Sahra.local>
2023-12-15 16:43:16 +0100

    feat: Create  ScrollToTop function

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -13,6 +13,7 @@ import {
   ProductPage,
   ProductDetails,
 } from "../pages";
+import { ScrollToTop } from "../helpers/scroll";
 
 const Wrapper = styled.div({
   height: "100%",
@@ -32,6 +33,7 @@ export function AppRoutes() {
     <Wrapper>
       <ToastContainer />
       <BrowserRouter>
+      <ScrollToTop/>
         <Navbar />
         <PageContent>
           <Routes>
